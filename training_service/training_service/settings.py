39c8d961c1ae9e426ba9cbfda61b873c5255770e
Egor Alexandrov <dibilarts@gmail.com>
2025-05-20 22:51:45 +0700

    final commit

--- training_service/training_service/settings.py
+++ training_service/training_service/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.getenv("DJANGO_TRAINING_SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = ['training-service', '127.0.0.1']
+ALLOWED_HOSTS = ['training-service', '127.0.0.1', '87.228.83.10']
 
 
 # Application definition
--- user_service/user_service/settings.py
+++ user_service/user_service/settings.py
@@ -22,9 +22,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = os.getenv('DJANGO_SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = ['user-service', '127.0.0.1']
+ALLOWED_HOSTS = ['user-service', '127.0.0.1', '87.228.83.10']
 
 
 # Application definition
