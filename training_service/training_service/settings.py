636703df76323928c93bb7fff7ecd04ee56fbc3a
Egor Alexandrov <dibilarts@gmail.com>
2025-05-22 11:33:25 +0700

    final commit

--- training_service/training_service/settings.py
+++ training_service/training_service/settings.py
@@ -31,7 +31,7 @@ ALLOWED_HOSTS = ['training-service', '127.0.0.1', '87.228.83.10']
 # Application definition
 
 INSTALLED_APPS = [
-    "django.contrib.admin",
+    #"django.contrib.admin",
     "django.contrib.auth",
     "django.contrib.contenttypes",
     "django.contrib.sessions",
